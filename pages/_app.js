67687bbc546b158319ccf0dbd5360294479fdd2b
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-07-03 16:27:51 +0500

    server change

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../src/Assets/scss/main.scss";
 import { wrapper } from "../store/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} Url="http://46.101.231.43:7070"/>;
+  return <Component {...pageProps} Url="https://gifster.mesi.pro"/>;
 }
 
 export default wrapper.withRedux(MyApp);
